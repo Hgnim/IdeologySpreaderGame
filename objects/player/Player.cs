4d439b77387ffe60921ee6103bbc48dc45500aa9
Hgnim <hgn1m@outlook.com>
2026-08-20 23:42:26 +0800

    代码提交

---
+++ objects/player/Player.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class Player : EntityBase 
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	/// <summary>
+	/// 移动推力
+	/// </summary>
+	[Export] public float moveForce = 1500f;
+	/// <summary>
+	/// 最大速度
+	/// </summary>
+	[Export] public float maxSpeed = 300f;
+	public override void _Process(double delta)
+	{
+		Vector2 keyInput = Input.GetVector("moveLeft", "moveRight", "moveUp", "moveDown");
+
+		if (keyInput != Vector2.Zero) {//按键按下时施加推力移动
+			ApplyCentralForce(keyInput * moveForce);
+		}
+
+		if (LinearVelocity.Length() > maxSpeed) {//限制最大速度，防止无限加速
+			LinearVelocity = LinearVelocity.Normalized() * maxSpeed;
+		}
+	}
+}
