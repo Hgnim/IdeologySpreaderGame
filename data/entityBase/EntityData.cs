2254ed08bd5f542d72209b8d39cb215cdea977e9
Hgnim <hgn1m@outlook.com>
2026-08-21 22:58:33 +0800

    代码提交

---
+++ data/entityBase/EntityData.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace IdeologySpreaderGame.data.entityBase {
+	internal enum Ideology {
+		none,
+		Anarchism,
+		Fascism,
+	}
+	internal class EntityData {
+		private Ideology ideology=Ideology.none;
+		internal Action<Ideology> Ideology_Change;
+		/// <summary>
+		/// 意识形态
+		/// </summary>
+		internal Ideology Ideology {
+			get => ideology;
+			set {
+				ideology = value;
+				Ideology_Change?.Invoke(Ideology);
+			}
+		}
+
+		private uint exp = 0;
+		internal Action<uint> Exp_Change;
+		/// <summary>
+		/// 经验
+		/// </summary>
+		internal uint Exp {
+			get => exp;
+			set {
+				exp = value;
+				Exp_Change?.Invoke(Exp);
+			}
+		}
+	}
+}
--- objects/entityBase/EntityBase.cs
+++ objects/entityBase/EntityBase.cs
@@ -1,15 +1,41 @@
 using Godot;
+using IdeologySpreaderGame.data.entityBase;
 using System;
 
 public abstract partial class EntityBase : RigidBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	/// <summary>
+	/// 默认使用的意识形态
+	/// </summary>
+	[Export]
+	internal Ideology defIdeology = Ideology.none;
+
+	internal EntityData EData { get; set; } = new();
+
+	internal Sprite2D ideology;
+	internal Label exp;
+	public override void _Ready() {
+		ideology = GetNode<Sprite2D>("ideology");
+		exp = GetNode<Label>("exp");
+
+		EData.Ideology_Change += Ideology_Change;
+		EData.Exp_Change += Exp_Change;
+
+		EData.Ideology = defIdeology;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	void Ideology_Change(Ideology ideo) {
+		switch (ideo) {
+			case Ideology.none:
+				ideology.Texture = null;
+				break;
+			case Ideology.Anarchism:
+				ideology.Texture = GD.Load<Texture2D>("res://assets/img/ideology/anarchism.png");
+				break;
+			case Ideology.Fascism:
+				ideology.Texture = GD.Load<Texture2D>("res://assets/img/ideology/fascism.png");
+				break;
+		}
 	}
+	void Exp_Change(uint exp) => this.exp.Text = exp.ToString();
 }
